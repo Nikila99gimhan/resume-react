a54f415c91312304dadeb9e4d733f3fc4854efad
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-03-07 19:09:18 +0530

    timeline section css start

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,40 +1,48 @@
-import React from "react";
+import React, { Children } from "react";
 import Timeline from "@material-ui/lab/Timeline";
 import TimelineItem from "@material-ui/lab/TimelineItem";
 import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
 import TimelineConnector from "@material-ui/lab/TimelineConnector";
 import TimelineContent from "@material-ui/lab/TimelineContent";
 import TimelineDot from "@material-ui/lab/TimelineDot";
+import WorkIcon from "@material-ui/icons/Work";
 
 import "./Timeline.css";
-import { Title } from "@material-ui/icons";
 import { Typography } from "@material-ui/core";
 
-const CostomTimeline = ({ title }) => {
+const CostomTimeline = ({ title, icon, children }) => {
   return (
     <Timeline className={"timeline"}>
       {/*Item Header */}
       <TimelineItem className={"timeline_firstitem"}>
         <TimelineSeparator>
-          <TimelineDot className={"timelinee_dot_header"} />
+          <TimelineDot className={"timeline_dot_header"}>
+            {<WorkIcon />}
+          </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent>
           <Typography variant="h6" className={"timeline_header"}>
-            {title}
+            {"title"}
           </Typography>
         </TimelineContent>
       </TimelineItem>
+
+      {children}
+
+      {/*Remaining Items */}
       <TimelineItem>
-        {/*Remaining Items */}
-        <TimelineSeparator>
-          <TimelineDot className={"timelinee_dot"} />
-          <TimelineConnector />
-        </TimelineSeparator>
+        <CostomTimelineSeparator />
         <TimelineContent>Code</TimelineContent>
       </TimelineItem>
     </Timeline>
   );
 };
 
+export const CostomTimelineSeparator = () => (
+  <TimelineSeparator className={"separator_padding"}>
+    <TimelineDot className={"timelinee_dot"} />
+    <TimelineConnector />
+  </TimelineSeparator>
+);
 export default CostomTimeline;
