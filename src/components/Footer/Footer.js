d16c27d666ca1d2771cf4e19b24a9357b3cbc27a
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-03-10 18:16:32 +0530

    Completed footer section with css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
       <di className="footer">
         <div className="footer_left">
-          <Typography className="footer_name"></Typography>
+          <Typography className="footer_name">{resumeData.name}</Typography>
         </div>
         <div className="footer_right">
           <Typography className="footer_copyright">
