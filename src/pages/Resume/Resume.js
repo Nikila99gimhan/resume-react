f5c976a06001869109911d7e3726fe10e50553a2
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2021-03-11 11:33:41 +0530

    modified app.css

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Resume.css';
-import {Grid} from "@material-ui/core";
+import {Grid, Typography} from "@material-ui/core";
+import resumeData from "../../utils/resumeData";
 
 const Resume = () => {
   return (
@@ -8,24 +9,31 @@ const Resume = () => {
       <>
         {/* About me*/}
 
-        <Grid container className=''></Grid>
+        <Grid container className=''>
+            <Grid item className='section_title' >
+                <span></span>
+                <h6>About Me</h6>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography className={'aboutme_text'}>{resumeData.about}</Typography>
+            </Grid>
+        </Grid>
 
-        {/* Educational And Experiences*/}
 
-
-        <Grid container className=''></Grid>
+          {/* Educational And Experiences*/}
+        <Grid container className='section'></Grid>
 
         {/* Services*/}
 
-        <Grid container className=''></Grid>
+        <Grid container className='section'></Grid>
 
         {/*Skills*/}
 
-        <Grid container className=''></Grid>
+        <Grid container className='section'></Grid>
 
         {/*Contact*/}
 
-        <Grid container className=''></Grid>
+        <Grid container className='section'></Grid>
 
 
       </>
--- src/utils/resumeData.js
+++ src/utils/resumeData.js
@@ -33,4 +33,8 @@ export default {
       icon: <GitHubIcon />,
     },
   },
+
+  about:"Hi! My name is Nikila Fernando , I am a second-year Computer Science student at NSBM Green University SriLanka.Started earnest brother believe an exposed so. Me he believing daughters if forfeited at furniture. Age again and stuff downs spoke. Late hour new nay able fat each sell. Nor themselves age introduced frequently use unsatiable devonshire get. They why quit gay cold rose deal park. One same they four did ask busy. Reserved opinions fat him nay position. Breakfast as zealously incommode do agreeable furniture. One too nay led fanny allow plate.\n\n" +
+      "Quick six blind smart out burst. Perfectly on furniture dejection determine my depending an to. Add short water court fat. Her bachelor honoured perceive securing but desirous ham required. Questions deficient acuteness to engrossed as. Entirely led ten humoured greatest and yourself. Besides ye country on observe. She continue appetite endeavor she judgment interest the met. For she surrounded motionless fat resolution may."
+
 };
